754a11f3194f0e705b494f172379b472d82b5ffe
oscar andres vargas madrid <oscar.andres.vargas1@gmail.com>
2021-10-04 12:28:36 +0200

    Init commit

---
+++ nivell1.js
@@ -0,0 +1,65 @@
+//----------------------------------------NIVELL 1----------------------------------------
+
+// Crear una funció que imprimeixi en consola el nom d'usuari sent invocada externament i passant
+// el nom com a paràmetre.
+
+
+const returnName = (nom) => nom
+
+console.log(returnName("oscar"))    
+
+const returnSecondName = nom => {
+    console.log("My name is " + nom)
+}
+
+returnSecondName("Oscar")
+
+//----------------------------------------NIVELL 2----------------------------------------
+// - Exercici 1
+// Imprimir el nom i cognoms de l'usuari en variables mitjançant template literals, creant les variables
+// i referenciant en la impressió de l'missatge
+
+const nom = 'Oscar'
+
+const cognom = 'Vargas'
+
+console.log(`Jo em dic ${nom} ${cognom}`)
+
+// - Exercici 2
+// Invocar la funció mitjançant template literals
+
+const printName = (nom, cognom) => {
+    console.log(`Jo em dic ${nom} ${cognom}`)
+}
+
+printName(nom, cognom)
+
+
+//----------------------------------------NIVELL 3----------------------------------------
+
+// - Exercici 1
+// Crea una matriu de deu funcions i empleni-la mitjançant un bucle. Cada funció comptarà del 0-9 imprimint-ho
+// per pantalla. Invoqui cada funció de l'array iterativament. Haurà d'imprimir-se per pantalla el compte de 
+// 0-9 deu vegades
+
+
+const fulfill = () => {
+    for (let i = 0; i < 10; i++) {
+        console.log(i)        
+    }
+}
+
+const matriu = []
+
+for (let i = 0; i < 10; i++) {
+    matriu[i] = fulfill()
+}
+
+
+// - Exercici 2
+// Crear una funció anònima autoinvocable (igualada a una variable) que imprimeixi per pantalla
+//  el nom d'usuari rebut com a paràmetre
+
+(function(nom) {
+    console.log(nom)
+})("Oscar")
